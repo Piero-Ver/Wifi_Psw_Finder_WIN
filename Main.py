df47a4d537505fda7d567100fd110f4807246a88
Piero Vergari <pierover268@gmail.com>
2022-08-26 13:14:02 +0200

    test

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import os
 
+print("Per vedere i nomi delle reti vicino a te lancia questo comando da cmd -> netsh wlan show profiles ")
+
 net=input("Inserisci il nome della rete -> ")
 print("L'output della rete "+net+ " è disponibile di seguito per la password cerca la voce 'Contenuto chiave' :} -> \n")
 os.system('cmd /k netsh wlan show profile name='+net+' key=clear')
